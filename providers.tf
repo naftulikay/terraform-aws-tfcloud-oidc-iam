2b5fcc513ab51f74f9a712df68f7dd9234d76da3
Naftuli Kay <me@naftuli.wtf>
2024-04-22 10:15:03 -0700

    specify versions

--- providers.tf
+++ providers.tf
@@ -6,9 +6,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 5"
     }
     tls = {
         source = "hashicorp/tls"
+        version = "~> 4"
     }
   }
 }
