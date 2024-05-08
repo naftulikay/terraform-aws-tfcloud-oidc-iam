ebef075fe760e02905302a43509e7045d2b09c13
Naftuli Kay <me@naftuli.wtf>
2024-05-08 08:14:55 -0700

    remove

--- r.iam.tf
+++ r.iam.tf
@@ -2,9 +2,6 @@
 
 resource aws_iam_openid_connect_provider default {
   url = var.url
-  # FIXME this is the original value copied from the gha module
-  # client_id_list = var.sts_endpoints
-  # this is what terraform cloud docs suggest
   client_id_list = ["aws.workload.identity"]
   thumbprint_list = length(var.thumbprint_list) == 0 ? [local.tfcloud_oidc_fingerprint] : var.thumbprint_list
   tags = var.tags
